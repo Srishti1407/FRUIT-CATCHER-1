33c5c30e07ccb19eb412c000a19ffa60b998cead
Srishti1407 <73740987+Srishti1407@users.noreply.github.com>
2021-01-20 22:56:35 +0530

    Add files via upload

--- Form.js
+++ Form.js
@@ -1,9 +1,10 @@
 class Form{
     constructor(){
-       this.input = createInput("Name");
-       this.button = createButton('Play');
-       this.greeting = createElement('h2');
-       this.title = createElement('h2');
+       //creating properties
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
     }
     hide() {
         this.greeting.hide();
@@ -12,18 +13,11 @@ class Form{
         this.title.hide();
     }
     display() {
-        this.title.html("FRUIT CATCHER");
-        this.title.position(350, 50);
-        this.title.style('font-size', '70px');
-        this.title.style('color', 'skyblue');
-        this.input.position(550,400);
-        this.input.style('width', '200px');
-        this.input.style('height', '20px');
-        this.input.style('background', 'lavender');
-        this.button.position(560,500);
-        this.button.style('width', '200px');
-        this.button.style('height', '40px');
-        this.button.style('background', 'lightpink');
+        this.title.html("Car Racing Game");
+        this.title.position(displayWidth/2 - 50, 0);
+    
+        this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+        this.button.position(displayWidth/2 + 30, displayHeight/2);
 
         this.button.mousePressed(() => {
             this.input.hide();
